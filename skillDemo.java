22bfe93e7d525d4618119dfbbbfd15b7f50d2f28
molan0426 <molanli2003@gmail.com>
2022-05-01 14:38:45 -0700

    add a test file and a lib folder

--- skillDemo.java
+++ skillDemo.java
@@ -1,1 +1,5 @@
-
+public class skillDemo{
+    public static boolean isPositive(int num){
+        return num > 0;
+    }
+}
---
+++ skillDemoTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+
+import java.beans.Transient;
+
+import org.junit.*;
+
+public class skillDemoTest {
+    
+    @Test
+    public void testIsPositive(){
+        assertTrue(skillDemo.isPositive(-3));
+    }
+}
